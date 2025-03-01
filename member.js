0f6af27615c22492ea2eab4f90471d41a9f520f7
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-03-01 13:29:11 +0000

    Copitlot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return {
+    title: 'Member',
+    description: 'Member description',
+    skills: [
+      {
+        title: 'Skill 1',
+        description: 'Skill 1 description',
+        level: 'Intermediate',
+      },
+      {
+        title: 'Skill 2',
+        description: 'Skill 2 description',
+        level: 'Beginner',
+      },
+    ],
+  };
+}
